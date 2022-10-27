a1800c977dfaa1abb5bcc56bac2a23344a3bc50e
Darfune <harolep@gmail.com>
2022-10-27 15:44:43 +0300

    api init

---
+++ app/src/main/java/com/example/recipesapp/network/RecipesApi.kt
@@ -0,0 +1,13 @@
+package com.example.recipesapp.network
+
+import com.example.recipesapp.models.meals.ListOfMeals
+import retrofit2.http.GET
+import javax.inject.Singleton
+
+@Singleton
+interface RecipesApi {
+
+    @GET("search.php?s=")
+    suspend fun getRecipes(): ListOfMeals
+
+}
