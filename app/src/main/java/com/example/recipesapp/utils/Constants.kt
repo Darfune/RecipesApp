310cded45d5e02c8e1952dbe3cd4926220010540
Darfune <harolep@gmail.com>
2022-10-27 14:59:36 +0300

    base url val

--- app/src/main/java/com/example/recipesapp/utils/Constants.kt
+++ app/src/main/java/com/example/recipesapp/utils/Constants.kt
@@ -3,8 +3,7 @@ package com.example.recipesapp.utils
 class Constants {
 
     companion object{
-        const val BASE_URL = "www.themealdb.com/api/json/v1/1/" +
-                ""
+        const val BASE_URL = "www.themealdb.com/api/json/v1/1/"
     }
 
 }
