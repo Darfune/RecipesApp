19a57449759b61ff22f2b4b6146f2b67f055c643
Darfune <harolep@gmail.com>
2022-10-31 22:37:23 +0200

    Added database and dao @Provides

--- app/src/main/java/com/example/recipesapp/di/AppModule.kt
+++ app/src/main/java/com/example/recipesapp/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.recipesapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.recipesapp.database.MealsDatabase
 import com.example.recipesapp.network.RecipesApi
 import com.example.recipesapp.utils.Constants
+import com.example.recipesapp.utils.Constants.Companion.DATABASE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,13 +19,27 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    // Retrofit
     @Singleton
     @Provides
-    fun provideBookApi(): RecipesApi {
-        return Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(RecipesApi::class.java)
-    }
+    fun provideBookApi(): RecipesApi = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(RecipesApi::class.java)
+
+    //Room
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        MealsDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: MealsDatabase) = database.mealsDao()
 }
