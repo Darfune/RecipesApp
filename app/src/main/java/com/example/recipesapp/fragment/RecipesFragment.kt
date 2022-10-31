36b2429da457e186dc9dd22cf6cc72fc94d28c06
Darfune <harolep@gmail.com>
2022-10-31 18:17:34 +0200

    Added shimmer effect

--- app/src/main/java/com/example/recipesapp/fragment/RecipesFragment.kt
+++ app/src/main/java/com/example/recipesapp/fragment/RecipesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,6 +15,8 @@ import com.example.recipesapp.MainViewModel
 import com.example.recipesapp.R
 import com.example.recipesapp.adapters.MealsAdapter
 import com.example.recipesapp.data.Resource
+import com.example.recipesapp.models.meals.ListOfMeals
+import com.facebook.shimmer.ShimmerFrameLayout
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,7 +25,11 @@ class RecipesFragment : Fragment() {
     private lateinit var mealView: View
     private lateinit var mainViewModel: MainViewModel
     private val mealsAdapter by lazy { MealsAdapter() }
+
     private lateinit var recyclerView: RecyclerView
+    private lateinit var shimmerLayout: ShimmerFrameLayout
+    private lateinit var noConnectionTextView: TextView
+    private lateinit var noConnectionImageView: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,9 +37,15 @@ class RecipesFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         mealView = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        shimmerLayout = mealView.findViewById(R.id.mealsShimmerLayout)
+
         recyclerView = mealView.findViewById(R.id.mealsRecyclerView)
         recyclerView.adapter = mealsAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        noConnectionTextView = mealView.findViewById(R.id.noConnectionTextView)
+        noConnectionImageView = mealView.findViewById(R.id.noConnectionImageView)
+
 
         mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
 
@@ -45,26 +59,39 @@ class RecipesFragment : Fragment() {
         mainViewModel.mealsResponse.observe(viewLifecycleOwner) { response ->
             when (response) {
                 is Resource.Success -> {
-                    response.data?.let { mealsAdapter.setData(it) }
+
+                    setupRecyclerView(response)
                 }
                 is Resource.Error -> {
-                    Toast.makeText(
-                        requireContext(),
-                        response.message.toString(),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    showErrorView(response)
+
+
                 }
                 is Resource.Loading -> {
-                    //TODO if possible me the shimmer effect
+
                 }
             }
 
         }
     }
 
-//    private fun setupRecyclerView() {
-//        recyclerView.showS
-//
-//    }
+    private fun showErrorView(response: Resource.Error<ListOfMeals>) {
+        shimmerLayout.stopShimmer()
+        shimmerLayout.visibility = View.GONE
+        noConnectionTextView.visibility = View.VISIBLE
+        noConnectionImageView.visibility = View.VISIBLE
+        Toast.makeText(
+            requireContext(),
+            response.message.toString(),
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun setupRecyclerView(response: Resource.Success<ListOfMeals>) {
+        shimmerLayout.stopShimmer()
+        shimmerLayout.visibility = View.GONE
+        response.data?.let { mealsAdapter.setData(it) }
+        recyclerView.visibility = View.VISIBLE
+    }
 
 }
