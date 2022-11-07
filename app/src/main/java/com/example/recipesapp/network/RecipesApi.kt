35c899771b9a43147d1c89639017701d808ca386
Darfune <harolep@gmail.com>
2022-11-07 17:00:40 +0200

    new api call and altered old one

--- app/src/main/java/com/example/recipesapp/network/RecipesApi.kt
+++ app/src/main/java/com/example/recipesapp/network/RecipesApi.kt
@@ -1,13 +1,23 @@
 package com.example.recipesapp.network
 
 import com.example.recipesapp.models.meals.ListOfMeals
+import com.example.recipesapp.models.mealssmall.MealsSmallList
 import retrofit2.http.GET
+import retrofit2.http.Query
 import javax.inject.Singleton
 
 @Singleton
 interface RecipesApi {
 
-    @GET("search.php?s=")
-    suspend fun getMeals(): ListOfMeals
+    @GET("search.php")
+    suspend fun getMeals(
+        @Query("s") nameOfMeal: String? = null
+    ): ListOfMeals
+
+    @GET("filter.php")
+    suspend fun getMealsByCategory(
+        @Query("c") category: String
+    ): MealsSmallList
+
 
 }
