ec439fb5cbc2998f7a3297638045ea746d73f7c6
Darfune <harolep@gmail.com>
2022-11-13 19:37:01 +0200

    Added functionality to top Searchbar and Loading Shimmer function

--- app/src/main/java/com/example/recipesapp/fragment/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/recipesapp/fragment/recipes/RecipesFragment.kt
@@ -2,11 +2,13 @@ package com.example.recipesapp.fragment.recipes
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -18,13 +20,15 @@ import com.example.recipesapp.adapters.MealsAdapter
 import com.example.recipesapp.data.Resource
 import com.example.recipesapp.databinding.FragmentRecipesBinding
 import com.example.recipesapp.models.meals.ListOfMeals
+import com.example.recipesapp.utils.NetworkListener
 //import com.example.recipesapp.repository.Requests
 import com.example.recipesapp.utils.observeOnce
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
@@ -32,6 +36,8 @@ class RecipesFragment : Fragment() {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var mealsViewModel: MealsViewModel
     private val mealsAdapter by lazy { MealsAdapter() }
+
+    private lateinit var networkListener: NetworkListener
 
     private val args by navArgs<RecipesFragmentArgs>()
 
@@ -50,15 +56,75 @@ class RecipesFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.mainViewModel = mainViewModel
 
+        setMenuProvider()
+
         initRecyclerView()
-        readDatabase()
+
+        mealsViewModel.readBackOnline.observe(viewLifecycleOwner) {
+            mealsViewModel.backOnline = it
+        }
+
+        lifecycleScope.launch {
+            networkListener = NetworkListener()
+            networkListener.checkNetworkAvailability(requireContext())
+                .collect { status ->
+                    Log.d("RecipesFragment", "NetworkListener: $status")
+                    mealsViewModel.networkStatus = status
+                    mealsViewModel.showNetworkStatus()
+                    readDatabase()
+                }
+        }
 
         binding.mealsTypeFloatingButton.setOnClickListener {
-            findNavController().navigate(R.id.action_recipesFragment_to_mealsBottomSheet)
+            if (mealsViewModel.networkStatus)
+                findNavController().navigate(R.id.action_recipesFragment_to_mealsBottomSheet)
+            else mealsViewModel.showNetworkStatus()
         }
 
         return binding.root
     }
+
+    private fun setMenuProvider() {
+        // The usage of an interface lets you inject your own implementation
+        val menuHost: MenuHost = requireActivity()
+
+        // Add menu items without using the Fragment Menu APIs
+        // Note how we can tie the MenuProvider to the viewLifecycleOwner
+        // and an optional Lifecycle.State (here, RESUMED) to indicate when
+        // the menu should be visible
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.meals_menu, menu)
+
+                val search = menu.findItem(R.id.menu_search)
+                val searchView = search.actionView as? SearchView
+                searchView?.isSubmitButtonEnabled = true
+                searchView?.setOnQueryTextListener(this@RecipesFragment)
+            }
+
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+    }
+
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (query != null) searchApiData(query)
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.d("RecipesFragment", "onQueryTextChange: $newText")
+//        if (newText != null) searchApiData(newText)
+//        else requestApiData()
+        return true
+    }
+
 
     private fun initRecyclerView() {
         binding.mealsRecyclerView.adapter = mealsAdapter
@@ -86,9 +152,20 @@ class RecipesFragment : Fragment() {
             when (response) {
                 is Resource.Success -> setupRecyclerView(response)
                 is Resource.Error -> showErrorView(response)
-                is Resource.Loading -> {
+                is Resource.Loading -> loadShimmerEffect()
+            }
+        }
+    }
 
-                }
+    private fun searchApiData(searchMealsQuery: String) {
+
+        mainViewModel.searchMeals(mealsViewModel.applySearchQuery(searchQuery = searchMealsQuery))
+        Log.d("RecipesFragment", "requestApiData: called")
+        mainViewModel.searchedMealsResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Success -> setupRecyclerView(response)
+                is Resource.Error -> showErrorView(response)
+                is Resource.Loading -> loadShimmerEffect()
             }
         }
     }
@@ -123,6 +200,12 @@ class RecipesFragment : Fragment() {
         } else {
             loadDataFromCache()
         }
+    }
+
+    private fun loadShimmerEffect() {
+        binding.mealsRecyclerView.visibility = View.GONE
+        binding.mealsShimmerLayout.startShimmer()
+        binding.mealsShimmerLayout.visibility = View.VISIBLE
     }
 
     override fun onDestroy() {
