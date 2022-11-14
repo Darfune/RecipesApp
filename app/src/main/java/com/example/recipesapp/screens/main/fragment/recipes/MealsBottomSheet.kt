7f2c715ada9abc97fa52ff671488dada603c305c
Darfune <harolep@gmail.com>
2022-11-14 22:06:12 +0200

    bug fix

--- app/src/main/java/com/example/recipesapp/screens/main/fragment/recipes/MealsBottomSheet.kt
+++ app/src/main/java/com/example/recipesapp/screens/main/fragment/recipes/MealsBottomSheet.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
 import com.example.recipesapp.R
 import com.example.recipesapp.databinding.MealsBottomSheetBinding
+import com.example.recipesapp.fragment.recipes.MealsBottomSheetDirections
 import com.example.recipesapp.utils.Constants.Companion.DEFAULT_MEAL_CATEGORY
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.chip.Chip
