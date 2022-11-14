896e7ebc0f8757478eff82d45580e1976f668fa4
Darfune <harolep@gmail.com>
2022-11-14 22:09:43 +0200

    DetailsActivity with pagerAdapter

---
+++ app/src/main/java/com/example/recipesapp/screens/details/DetailsActivity.kt
@@ -0,0 +1,69 @@
+package com.example.recipesapp.screens.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
+import com.example.recipesapp.R
+import com.example.recipesapp.adapters.PagerAdapter
+import com.example.recipesapp.databinding.ActivityDetailsBinding
+import com.example.recipesapp.screens.details.fragments.ingredients.IngredientsFragment
+import com.example.recipesapp.screens.details.fragments.instructions.InstructionsFragment
+import com.example.recipesapp.screens.details.fragments.overview.OverviewFragment
+import com.google.android.material.tabs.TabLayoutMediator
+
+class DetailsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailsBinding
+
+    private val args by navArgs<DetailsActivityArgs>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("mealBundle", args.meal)
+
+        val pagerAdapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            this
+        )
+
+        binding.viewPager2.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+            tab.text = titles[position]
+        }.attach()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
