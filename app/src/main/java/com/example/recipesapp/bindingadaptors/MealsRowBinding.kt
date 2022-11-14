4d878e7a9038f0299a135bcac4a82398de3ebae6
Darfune <harolep@gmail.com>
2022-11-14 22:06:34 +0200

    navigation to DetailsActivity

--- app/src/main/java/com/example/recipesapp/bindingadaptors/MealsRowBinding.kt
+++ app/src/main/java/com/example/recipesapp/bindingadaptors/MealsRowBinding.kt
@@ -1,13 +1,31 @@
 package com.example.recipesapp.bindingadaptors
 
+import android.util.Log
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.example.recipesapp.R
+import com.example.recipesapp.models.meals.Meal
+import com.example.recipesapp.screens.main.fragment.recipes.RecipesFragmentDirections
 
 class MealsRowBinding {
 
     companion object {
+
+        @BindingAdapter("onMealClickListener")
+        @JvmStatic
+        fun onMealClickListener(mealRowLayout: ConstraintLayout, meal: Meal) {
+            mealRowLayout.setOnClickListener {
+                try {
+                    val action = RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(meal)
+                    mealRowLayout.findNavController().navigate(action)
+                } catch (exc: Exception){
+                    Log.d("onMealClickListener: ", exc.toString())
+                }
+            }
+        }
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
