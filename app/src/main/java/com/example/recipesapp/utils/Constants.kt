0e460b7aa7051e3cc5dc4b1a2c81a54734e17482
Darfune <harolep@gmail.com>
2022-11-22 16:41:38 +0200

    new url const

--- app/src/main/java/com/example/recipesapp/utils/Constants.kt
+++ app/src/main/java/com/example/recipesapp/utils/Constants.kt
@@ -5,6 +5,8 @@ class Constants {
     companion object{
         // Retrofit
         const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+        const val BASE_ING_URL = "www.themealdb.com/images/ingredients/"
+
 
         // ROOM
         const val DATABASE_NAME = "recipes_database"
