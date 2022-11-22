4db2eaa6bfc30ceca897cd893bcb2548b43e844e
Darfune <harolep@gmail.com>
2022-11-22 21:00:27 +0200

    Ingredients Fragment Finished

--- app/src/main/java/com/example/recipesapp/screens/details/fragments/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/example/recipesapp/screens/details/fragments/ingredients/IngredientsFragment.kt
@@ -5,16 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.recipesapp.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recipesapp.adapters.IngredientsAdapter
+import com.example.recipesapp.databinding.FragmentIngredientsBinding
+import com.example.recipesapp.models.ingredients.SmallIngredient
+import com.example.recipesapp.models.meals.Meal
 
 class IngredientsFragment : Fragment() {
+
+    private var _binding: FragmentIngredientsBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var ingredientsViewModel: IngredientsViewModel
+
+    private val ingredientsAdapter: IngredientsAdapter by lazy { IngredientsAdapter() }
+
+    private lateinit var ingredientList: List<SmallIngredient>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ingredientsViewModel = ViewModelProvider(requireActivity())[IngredientsViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        _binding = FragmentIngredientsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Meal = args!!.getParcelable<Meal>("mealBundle") as Meal
+
+        setupRecyclerView()
+        setupIngredientsList(myBundle)
+        ingredientList.let { ingredientsAdapter.setData(it) }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredients, container, false)
+        return binding.root
     }
 
+    private fun setupIngredientsList(myBundle: Meal) {
+        ingredientList = ingredientsViewModel.setupData(myBundle)
+    }
+
+
+    private fun setupRecyclerView(){
+        binding.ingredientsRecyclerView.adapter = ingredientsAdapter
+        binding.ingredientsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
