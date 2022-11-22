5804de6ba12792a537add0d96640e75b8317c82e
Darfune <harolep@gmail.com>
2022-11-22 16:41:05 +0200

    not needed

--- app/src/main/java/com/example/recipesapp/models/mealssmall/MealsSmallList.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recipesapp.models.mealssmall
-
-
-import com.google.gson.annotations.SerializedName
-
-data class MealsSmallList(
-    @SerializedName("mealSmalls")
-    val mealSmalls: List<MealsSmall>
-)
