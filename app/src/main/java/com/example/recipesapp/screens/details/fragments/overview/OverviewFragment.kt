620cb05714f96dc959a8abb36d48e7b630154e40
Darfune <harolep@gmail.com>
2022-11-22 13:36:36 +0200

    overview fragment finished

--- app/src/main/java/com/example/recipesapp/screens/details/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/recipesapp/screens/details/fragments/overview/OverviewFragment.kt
@@ -1,11 +1,16 @@
 package com.example.recipesapp.screens.details.fragments.overview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import coil.load
 import com.example.recipesapp.R
+import com.example.recipesapp.models.meals.Meal
 
 class OverviewFragment : Fragment() {
 
@@ -14,6 +19,14 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        val args = arguments
+        val myBundle: Meal = args!!.getParcelable<Meal>("mealBundle") as Meal
+
+        view.findViewById<ImageView>(R.id.mealMainImageView).load(myBundle.strImageSource)
+        view.findViewById<TextView>(R.id.selectedMealTitleTextView).text = myBundle.strMeal
+        view.findViewById<TextView>(R.id.descriptionOfMealTextView).text = myBundle.strInstructions
+        return view
     }
 }
