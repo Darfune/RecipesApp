62bf0b89801089f7b4c7bdc210aa445ba602eb73
Darfune <harolep@gmail.com>
2022-11-22 21:01:46 +0200

    comment future code

--- app/src/main/java/com/example/recipesapp/repository/RecipesRepository.kt
+++ app/src/main/java/com/example/recipesapp/repository/RecipesRepository.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import com.example.recipesapp.data.Resource
 import com.example.recipesapp.models.meals.ListOfMeals
 import com.example.recipesapp.models.meals.Meal
-import com.example.recipesapp.models.mealssmall.MealsSmallList
 import com.example.recipesapp.network.RecipesApi
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
@@ -23,34 +22,34 @@ class RecipesRepository @Inject constructor(
         return Resource.Success(response)
     }
 
-    suspend fun getMealsByCategory(category: String): Resource<MealsSmallList> {
-        val response = try {
-            recipesApi.getMealsByCategory(category)
-        } catch (exc: Exception) {
-            Log.d("Recipes Repository", "getMealsByCategory: $exc")
-            return Resource.Error("Unexpected Error occurred.")
-        }
-        return Resource.Success(response)
-    }
+//    suspend fun getMealsByCategory(category: String): Resource<MealsSmallList> {
+//        val response = try {
+//            recipesApi.getMealsByCategory(category)
+//        } catch (exc: Exception) {
+//            Log.d("Recipes Repository", "getMealsByCategory: $exc")
+//            return Resource.Error("Unexpected Error occurred.")
+//        }
+//        return Resource.Success(response)
+//    }
 
-    suspend fun getMealById(id: String): Resource<Meal> {
-        val response = try {
-            recipesApi.getMealById(id)
-        } catch (exc: Exception) {
-            Log.d("Recipes Repository", "getMealsByCategory: $exc")
-            return Resource.Error("Unexpected Error occurred.")
-        }
-        return Resource.Success(response)
-    }
-
-    suspend fun getRandomMeal(): Resource<Meal> {
-        val response = try {
-            recipesApi.getRandomMeal()
-        } catch (exc: Exception) {
-            Log.d("Recipes Repository", "getRandomMeal: $exc")
-            return Resource.Error("Unexpected Error occurred.")
-        }
-        return Resource.Success(response)
-    }
+//    suspend fun getMealById(id: String): Resource<Meal> {
+//        val response = try {
+//            recipesApi.getMealById(id)
+//        } catch (exc: Exception) {
+//            Log.d("Recipes Repository", "getMealsByCategory: $exc")
+//            return Resource.Error("Unexpected Error occurred.")
+//        }
+//        return Resource.Success(response)
+//    }
+//
+//    suspend fun getRandomMeal(): Resource<Meal> {
+//        val response = try {
+//            recipesApi.getRandomMeal()
+//        } catch (exc: Exception) {
+//            Log.d("Recipes Repository", "getRandomMeal: $exc")
+//            return Resource.Error("Unexpected Error occurred.")
+//        }
+//        return Resource.Success(response)
+//    }
 
 }
