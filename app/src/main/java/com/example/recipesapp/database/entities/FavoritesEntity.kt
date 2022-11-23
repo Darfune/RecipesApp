14f46e3eb8b97a84e1210fd73b7b0acfeed2edf4
Darfune <harolep@gmail.com>
2022-11-23 17:43:37 +0200

    favorites meals entity class

---
+++ app/src/main/java/com/example/recipesapp/database/entities/FavoritesEntity.kt
@@ -0,0 +1,13 @@
+package com.example.recipesapp.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.recipesapp.models.meals.Meal
+import com.example.recipesapp.utils.Constants.Companion.FAVORITES_MEALS_TABLE
+
+@Entity(tableName = FAVORITES_MEALS_TABLE)
+class FavoritesEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var meal: Meal
+)
