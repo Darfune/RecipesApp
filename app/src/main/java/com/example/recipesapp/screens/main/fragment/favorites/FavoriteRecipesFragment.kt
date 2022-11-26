aef791998ac0eb8de2d53cde70e89f8661a2e51d
Darfune <harolep@gmail.com>
2022-11-26 21:43:03 +0200

    can view favorite meals

--- app/src/main/java/com/example/recipesapp/screens/main/fragment/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/recipesapp/screens/main/fragment/favorites/FavoriteRecipesFragment.kt
@@ -5,16 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesapp.R
+import com.example.recipesapp.adapters.FavoriteMealsAdapter
+import com.example.recipesapp.databinding.FragmentFavoriteRecipesBinding
+import com.example.recipesapp.databinding.FragmentRecipesBinding
+import com.example.recipesapp.screens.main.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+
+
+    private val favoriteMealsAdapter: FavoriteMealsAdapter by lazy { FavoriteMealsAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+
+        setupRecyclerView(binding.favoriteMealsRecyclerView)
+
+        mainViewModel.readFavoriteMeals.observe(viewLifecycleOwner) { favoritesEntity ->
+            favoriteMealsAdapter.setData(favoritesEntity)
+
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        return binding.root
+    }
+
+    private fun setupRecyclerView(favoriteMealsRecyclerView: RecyclerView) {
+
     }
 
 }
