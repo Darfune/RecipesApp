fc829873f260b5b68207248dec996405c63afc0a
Darfune <harolep@gmail.com>
2022-11-29 15:44:18 +0200

    Single click view meal listener

--- app/src/main/java/com/example/recipesapp/adapters/FavoriteMealsAdapter.kt
+++ app/src/main/java/com/example/recipesapp/adapters/FavoriteMealsAdapter.kt
@@ -1,11 +1,16 @@
 package com.example.recipesapp.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet.Layout
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recipesapp.R
 import com.example.recipesapp.database.entities.FavoritesEntity
 import com.example.recipesapp.databinding.FavoritesRowLayoutBinding
+import com.example.recipesapp.screens.main.fragments.favorites.FavoriteRecipesFragmentDirections
 import com.example.recipesapp.utils.MealsDiffUtils
 
 class FavoriteMealsAdapter : RecyclerView.Adapter<FavoriteMealsAdapter.FavoriteMealsViewHolder>() {
@@ -32,8 +37,21 @@ class FavoriteMealsAdapter : RecyclerView.Adapter<FavoriteMealsAdapter.FavoriteM
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteMealsViewHolder =
         FavoriteMealsViewHolder.from(parent)
 
-    override fun onBindViewHolder(holder: FavoriteMealsViewHolder, position: Int) =
+    override fun onBindViewHolder(holder: FavoriteMealsViewHolder, position: Int) {
         holder.bind(favoriteMeals[position])
+
+
+        /**
+         * Single Click Listener
+         */
+        holder.itemView.findViewById<View>(R.id.favoriteMealssRowLayout).setOnClickListener {
+            val action =
+                FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(
+                    favoriteMeals[position].meal
+                )
+            holder.itemView.findNavController().navigate(action)
+        }
+    }
 
     override fun getItemCount(): Int = favoriteMeals.size
 
