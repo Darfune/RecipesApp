52c6322d3e8a3f1fe68ea80721faa40ff0046115
Darfune <harolep@gmail.com>
2022-11-30 13:53:37 +0200

    consts for flag images

--- app/src/main/java/com/example/recipesapp/utils/Constants.kt
+++ app/src/main/java/com/example/recipesapp/utils/Constants.kt
@@ -5,10 +5,16 @@ class Constants {
     companion object{
         // Retrofit
         const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+        // For Ingredients
         const val BASE_ING_IMG_URL = "https://www.themealdb.com/images/ingredients/"
         const val END_SMALL_IMG_URL = "-Small.png"
+        // For Flags
+        const val BASE_FLG_IMG_URL = "https://www.themealdb.com/images/icons/flags/big"
+        const val FLG_M_IMG_SIZE = "/64/"
+        const val FLG_L_IMG_SIZE = "/128/"
+        const val END_FLG_IMG_URL = ".png"
 
-        // ROOM
+            // ROOM
         const val DATABASE_NAME = "recipes_database"
         const val RECIPES_TABLE = "recipes_table"
         const val FAVORITES_MEALS_TABLE = "favorites_meals_table"
